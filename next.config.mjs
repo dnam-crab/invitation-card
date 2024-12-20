f0904141137eedef7d588edf9d04de5bfca08149
namdd <daonam230699@gmail.com>
2024-12-20 15:00:31 +0700

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Vì GitHub Pages không hỗ trợ xử lý ảnh động.
+  },
+  basePath: "/invitation-card",
+  assetPrefix: "/invitation-card",
+};
 
 export default nextConfig;
